f3ffc08a96dc251ac19b02ede75064753a40a139
Karl D. Asmussen <karl.d.asmussen@gmail.com>
2018-09-10 14:16:06 +0200

    Mon Sep 10 14:16:06 CEST 2018

--- 0/src/Arithmetic.hs
+++ 0/src/Arithmetic.hs
@@ -1,10 +1,6 @@
-{-# LANGUAGE FlexibleContexts #-}
 {-# OPTIONS_GHC -W #-}
+{-# LANGUAGE FlexibleContexts #-}
 
--- | Module      : Arithmetic
---   Description : Solution to Assignment 1
---   Copyright   : Karl D. Asmussen © Sep. 2018
---   License     : Public Domain
 module Arithmetic
   (
   showExp,
@@ -20,48 +16,13 @@ module Arithmetic
 where
 
 import Definitions
-
-showExp :: Exp -> String
-showExp = undefined
-
-evalSimple :: Exp -> Integer
-evalSimple = undefined
-
-extendEnv :: VName -> Integer -> Env -> Env
-extendEnv = undefined
-
-evalFull :: Exp -> Env -> Integer
-evalFull = undefined
-
-evalErr :: Exp -> Env -> Either ArithError Integer
-evalErr = undefined
-
--- optional parts (if not attempted, leave them unmodified)
-
-showCompact :: Exp -> String
-showCompact = undefined
-
-evalEager :: Exp -> Env -> Either ArithError Integer
-evalEager = undefined
-
-evalLazy :: Exp -> Env -> Either ArithError Integer
-evalLazy = undefined
-
 import Control.Monad
 import Control.Monad.Except
 import Control.Monad.Reader
 
--- | Simple rendering for 'ArithError'
-showErr (EBadVar n) = "Variable not found: `" ++ n ++ "'"
-showErr EDivZero = "Division by zero"
-showErr ENegPower = "Negative exponent"
-showErr (EOther e) = if null e then "Error" else "Error: " ++ e
-
--- | Simple rendering for 'Exp'
 showExp :: Exp -> String
 showExp e = showsExp e ""
-
-  where -- | Using `ShowS` for convenience
+  where -- Using `ShowS` for convenience
         showsExp :: Exp -> ShowS
         showsExp (Cst i) = shows i
         showsExp (Add l r) = parInfixsExp l "+" r
@@ -71,44 +32,121 @@ showExp e = showsExp e ""
         showsExp (Pow l r) = parInfixsExp l "^" r
         showsExp _ = error "unsupported Exp constructor in showExp"
         
-        -- | 'ShowS' conbinator for enclosing in parentheses
+        -- 'ShowS' conbinator for enclosing in parentheses
         parens :: ShowS -> ShowS
         parens s = ('(':) . s . (')':)
 
-        -- | 'ShowS' combinator for infix operators
+        -- 'ShowS' combinator for infix operators
         infixs :: ShowS -> String -> ShowS -> ShowS
-        infixs l o r = l . (' ':) . (o ++) . (' ':) . r
+        infixs l o r = l . (o ++) . r
 
-        -- | Utility function converting 'Exp' to 'ShowS'
+        -- Utility function converting 'Exp' to 'ShowS'
         infixsExp :: Exp -> String -> Exp -> ShowS
         infixsExp l o r = infixs (showsExp l) o (showsExp r)
         
-        -- | Utility function combining 'infixsExp' and 'parens'
+        -- Utility function combining 'infixsExp' and 'parens'
         parInfixsExp :: Exp -> String -> Exp -> ShowS
         parInfixsExp l o r = parens (infixsExp l o r)
 
--- | Simple evaluator for 'Exp'
+
 evalSimple :: Exp -> Integer
-evalSimple (Cst i) = i
-evalSimple (Add l r) = evalSimple l + evalSimple r
-evalSimple (Sub l r) = evalSimple l - evalSimple r
-evalSimple (Mul l r) = evalSimple l * evalSimple r
-evalSimple (Div l r) = evalSimple l `div` evalSimple r
-evalSimple (Pow l r) = evalSimple l ^ evalSimple r
-evalSimple _ = error "unsupported Exp constructor in showExp"
+evalSimple e = evalFull e (error "unsupported Exp constructor in showExp")
 
--- | Variable environment alias as given
-type Env = VName -> Maybe Integer
+-- evalSimple (Cst i) = i
+-- evalSimple (Add l r) = evalSimple l + evalSimple r
+-- evalSimple (Sub l r) = evalSimple l - evalSimple r
+-- evalSimple (Mul l r) = evalSimple l * evalSimple r
+-- evalSimple (Div l r) = evalSimple l `div` evalSimple r
+-- evalSimple (Pow l r) = evalSimple l ^ evalSimple r
+-- evalSimple _ = error "unsupported Exp constructor in showExp"
 
--- | Empty environment
-initEnv :: Env
-initEnv = \_ -> Nothing
-
--- | Utility function for extending environment with single variable
 extendEnv :: VName -> Integer -> Env -> Env
-extendEnv v i e = \v' -> if v == v' then Just i else e v'
+extendEnv v i e = e `seq` \v' -> if v == v' then Just i else e v'
 
--- | Monadic evaluator
+evalFull :: Exp -> Env -> Integer
+evalFull exp env = case evalErr exp env of
+                        Left err -> error (showErr err)
+                        Right res -> res
+
+evalErr :: Exp -> Env -> Either ArithError Integer
+evalErr exp env = runReaderT (evalM exp) env
+
+-- optional parts (if not attempted, leave them unmodified)
+
+showCompact :: Exp -> String
+showCompact exp = showsCompact exp 0 ""
+  where showsCompact :: Exp -> Integer -> ShowS
+        showsCompact (Cst i) _ = shows i
+        showsCompact (Let _ _ _) _ = undefined
+        showsCompact (Var _) _ = undefined
+        showsCompact (Sum _ _ _ _) _ = undefined
+        showsCompact exp prec = (if prec > lrp then parens else id) (ls . operator exp . rs)
+          where (le, re) = leftRight exp
+                (lp, rp)      = precedence exp
+                ls = showsCompact le lp
+                rs = showsCompact re rp
+                lrp          = min lp rp
+        
+        leftRight :: Exp -> (Exp, Exp)
+        leftRight (Add l r) = (l, r)
+        leftRight (Sub l r) = (l, r)
+        leftRight (Mul l r) = (l, r)
+        leftRight (Div l r) = (l, r)
+        leftRight (Pow l r) = (l, r)
+        leftRight _ = undefined
+
+        precedence :: Exp -> (Integer, Integer)
+        precedence (Add _ _) = (10, 10)
+        precedence (Sub _ _) = (10, 11)
+        precedence (Mul _ _) = (20, 20)
+        precedence (Div _ _) = (20, 21)
+        precedence (Pow _ _) = (31, 30)
+        precedence _ = undefined
+
+        parens :: ShowS -> ShowS
+        parens s = ('(':) . s . (')':)
+
+        operator :: Exp -> ShowS
+        operator (Add _ _) = ('+':)
+        operator (Sub _ _) = ('-':)
+        operator (Mul _ _) = ('*':)
+        operator (Div _ _) = ('/':)
+        operator (Pow _ _) = ('^':)
+        operator _ = undefined
+
+evalEager :: Exp -> Env -> Either ArithError Integer
+evalEager = evalErr
+
+evalLazy :: Exp -> Env -> Either ArithError Integer
+evalLazy (Cst i) _env = return i
+evalLazy (Add l r) env = liftM2 (+) (evalLazy l env) (evalLazy r env)
+evalLazy (Sub l r) env = liftM2 (-) (evalLazy l env) (evalLazy r env)
+evalLazy (Mul l r) env = liftM2 (*) (evalLazy l env) (evalLazy r env)
+evalLazy (Div l r) env = liftM2 div (evalLazy l env) (evalLazy r env)
+evalLazy (Pow l r) env = liftM2 (^) (evalLazy l env) (evalLazy r env)
+evalLazy (If c t f) env = do c' <- evalLazy c env
+                             if 0 == c'
+                                then evalLazy t env
+                                else evalLazy f env
+evalLazy (Let n a b) env = do a' <- evalLazy a env
+                              evalLazy b (extendEnv n a' env)
+evalLazy (Var n) env = case env n of
+                            Just i -> return i
+                            Nothing -> throwError (EBadVar n)
+evalLazy (Sum n f t b) env = do f' <- evalLazy f env
+                                t' <- evalLazy t env
+                                xs <- forM [f' .. t'] $ \i -> evalLazy b (extendEnv n i env)
+                                return (sum xs)
+
+-- utility functions
+
+-- Simple rendering for 'ArithError'
+showErr (EBadVar n) = "Variable not found: `" ++ n ++ "'"
+showErr EDivZero = "Division by zero"
+showErr ENegPower = "Negative exponent"
+showErr (EOther e) = if null e then "Error" else "Error: " ++ e
+
+-- Monadic evaluator
 evalM :: (MonadError ArithError m, MonadReader Env m) => Exp -> m Integer
 evalM (Cst i) = return i
 evalM (Add l r) = liftM2 (+) (evalM l) (evalM r)
@@ -138,30 +176,3 @@ evalM (Sum n f t b) = do f' <- evalM f
                          t' <- evalM t
                          xs <- forM [f' .. t'] $ \i -> local (extendEnv n i) (evalM b)
                          return (sum xs)
-
--- | Evaluate 'Exp' using variables
-evalFull :: Exp -> Env -> Integer
-evalFull exp env = case evalErr exp env of
-                        Left err -> error (showErr err)
-                        Right res -> res
-
--- | Evaluate 'Exp' in an exception-safe manner
-evalErr :: Exp -> Env -> Either ArithError Integer
-evalErr exp env = runReaderT (evalM exp) env
-
--- | Compactly render 'Exp'
-showCompact :: Exp -> String
-showComapct exp = showsCompact' exp 0
-  where showsCompact :: Exp -> Integer -> ShowS
-        showsCompact (Cst i) _ = shows i
-        showsCompact exp prec = _
-        
-        leftRight :: Exp -> (Exp, Exp)
-        leftRight = _
-        precedence :: Exp -> (Integer, Integer)
-        precedence (Cst _) = (100, 100)
-        precedence (Add _ _) = (10, 11)
-        precedence (Sub _ _) = (10, 11)
-        precedence (Mul _ _) = (20, 21)
-        precedence (Div _ _) = (20, 21)
-        precedence (Pow _ _) = (31, 30)
